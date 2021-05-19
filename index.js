6c9a2806303bcd6168bcce175e24b8be5141d332
Ishi2001 <71957301+Ishi2001@users.noreply.github.com>
2021-05-19 11:38:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var randomNumber1=Math.floor(Math.random()*6)+1;
 
-var randomDiceImage1="images/dice" + randomNumber1 + ".png";
+var randomDiceImage1="dice" + randomNumber1 + ".png";
 
 var image1=document.querySelectorAll("img")[0];
 
@@ -12,7 +12,7 @@ image1.setAttribute("src",randomDiceImage1);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
 
-var randomDiceImage2="images/dice" + randomNumber2 + ".png";
+var randomDiceImage2="dice" + randomNumber2 + ".png";
 
 var image2=document.querySelectorAll("img")[1];
 
